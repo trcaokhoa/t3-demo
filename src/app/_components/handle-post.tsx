456c19a732cc3323327a8233c99ee207b1543977
trinhcaokhoa <khoa@zenith-research.ca>
2023-11-16 15:47:38 -0800

    FIX isLoading update components

--- src/app/_components/handle-post.tsx
+++ src/app/_components/handle-post.tsx
@@ -1,4 +1,4 @@
-"use client";"use client";
+"use client"
 import { useState } from "react";
 import { api } from "~/trpc/react";
 
@@ -7,21 +7,32 @@ export function UpdatePost() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  const updatePost = api.post.updatePost.useMutation();
+  const updatePost = api.post.updatePost.useMutation({
+    onSuccess: () => {
+      // Handle success - maybe clear input fields or update state
+      setPostId("");
+      setTitle("");
+      setDescription("");
+    },
+    onError: (error) => {
+      // Handle error case
+      console.error("Error updating post:", error);
+    },
+  });
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     await updatePost.mutate({ id: postId, title, description });
-    // Handle success/error cases if needed
-    // Clear input fields or update state
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={postId} onChange={(e) => setPostId(e.target.value)} />
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-      <button type="submit">Update</button>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+      <input className="w-full rounded-full px-4 py-2 text-black" type="text" value={postId} onChange={(e) => setPostId(e.target.value)} />
+      <input className="w-full rounded-full px-4 py-2 text-black" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input className="w-full rounded-full px-4 py-2 text-black" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+      <button className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20" type="submit" disabled={updatePost.isLoading}>
+        {updatePost.isLoading ? "Updating..." : "Update"}
+      </button>
     </form>
   );
 }
