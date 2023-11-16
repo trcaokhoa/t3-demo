5f906c948356f241291aeb96857ee1335320e879
trinhcaokhoa <khoa@zenith-research.ca>
2023-11-16 15:39:28 -0800

    ADD crud for post router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -16,22 +16,23 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ title: z.string() }))
+    .input(z.object({ description: z.string() }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          title: input.title,
+          description: input.description,
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
     }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
+    return ctx.db.post.findMany({
       where: { createdBy: { id: ctx.session.user.id } },
     });
   }),
@@ -39,4 +40,27 @@ export const postRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+  // Update a post by ID
+  updatePost: protectedProcedure
+    .input(z.object({ id: z.string(), title: z.string(), description: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.post.update({
+        where: { id: input.id },
+        data: {
+          title: input.title,
+          description: input.description,
+        },
+      });
+    }),
+
+  // Delete a post by ID
+  deletePost: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.post.delete({
+        where: { id: input },
+      });
+    }),
+
 });
